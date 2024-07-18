ed4bd14db8979a1972d0bf608faa32c8ae0dcb14
Daniel Dytert <daniel.dytert@hanseaticbank.de>
2024-07-18 12:22:44 +0200

    get all taks for one user using sql query

--- internal/database/dbrepo/postgres.go
+++ internal/database/dbrepo/postgres.go
@@ -1,34 +1,62 @@
 package dbrepo
 
 import (
+	"context"
 	"database/sql"
 	"time"
 
 	"github.com/ddytert/todo/internal/models"
 )
 
-//const dbTimeout = time.Second * 3
+const dbTimeout = time.Second * 3
 
 func (m *postgresDBRepo) Connection() *sql.DB {
 	return m.DB
 }
 
 func (m *postgresDBRepo) AllTasksForUser(userID int) ([]*models.Task, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		select
+			id, task_list_id, user_id,
+			title, content, state_id,
+			priority_id, due_date,
+			created_at, updated_at
+		from
+			tasks
+		where user_id = $1
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
 	var tasks []*models.Task
 
-	dueDate := time.Now().AddDate(0, 0, 3)
+	for rows.Next() {
+		var t models.Task
+		err := rows.Scan(
+			&t.ID,
+			&t.TaskListID,
+			&t.UserID,
+			&t.Title,
+			&t.Content,
+			&t.StateID,
+			&t.PriorityID,
+			&t.DueDate,
+			&t.CreatedAt,
+			&t.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, &t)
+	}
 
-	tasks = append(tasks, &models.Task{
-		ID:         1,
-		TaskListID: 1,
-		UserID:     1,
-		Title:      "Take out the trash",
-		Content:    "My least favorite task",
-		StateID:    1,
-		PriorityID: 1,
-		DueDate:    dueDate,
-		CreatedAt:  time.Now(),
-		UpdatedAt:  time.Now(),
-	})
 	return tasks, nil
 }
