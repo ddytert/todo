0db62b0c86e79ee851f1ea81702d67327525386a
Daniel Dytert <daniel.dytert@hanseaticbank.de>
2024-07-18 12:23:45 +0200

    make dueDate a pointer to timestamp to make it nullable

--- internal/models/Task.go
+++ internal/models/Task.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type Task struct {
-	ID         int       `json:"id"`
-	TaskListID int       `json:"task_list_id"`
-	UserID     int       `json:"user_id"`
-	Title      string    `json:"title"`
-	Content    string    `json:"content"`
-	StateID    int       `json:"state_id"`
-	PriorityID int       `json:"priority_id"`
-	DueDate    time.Time `json:"due_date"`
-	CreatedAt  time.Time `json:"-"`
-	UpdatedAt  time.Time `json:"-"`
+	ID         int        `json:"id"`
+	TaskListID int        `json:"task_list_id"`
+	UserID     int        `json:"user_id"`
+	Title      string     `json:"title"`
+	Content    string     `json:"content"`
+	StateID    int        `json:"state_id"`
+	PriorityID int        `json:"priority_id"`
+	DueDate    *time.Time `json:"due_date"`
+	CreatedAt  time.Time  `json:"-"`
+	UpdatedAt  time.Time  `json:"-"`
 }
